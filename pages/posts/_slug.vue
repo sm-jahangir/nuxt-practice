9a7eb5a4615fe2951872d70fe2b28c06dea053c5
sm-jahangir <samraatjahangir@gmail.com>
2021-12-10 21:58:35 +0600

    Post by Single post

---
+++ pages/posts/_slug.vue
@@ -0,0 +1,37 @@
+<template>
+  <article>
+    <h1>{{ mountain.title }}</h1>
+    <section>
+      <!-- <p></p> -->
+      <div v-html="mountain.body"></div>
+    </section>
+    <button @click="goBack">Back</button>
+  </article>
+</template>
+<script>
+export default {
+  async asyncData({ $axios, params }) {
+    const mountain = await $axios.$get(
+      `http://localhost/laravel/mini-blog/api/posts/${params.slug}`
+    )
+    return { mountain }
+  },
+  methods: {
+    goBack() {
+      return this.$router.go(-1)
+    }
+  }
+}
+</script>
+<style scoped>
+article {
+  max-width: 800px;
+  margin: 0 auto;
+}
+img {
+  height: 200px;
+}
+p {
+  text-align: left;
+}
+</style>
---
+++ pages/posts/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="text-center">
+    <h1>Data fetched using asyncData</h1>
+    <ul>
+      <li v-for="post in posts" :key="post.id">
+        <NuxtLink :to="{ name: 'posts-slug', params: { slug: post.slug } }">
+          {{ post.title }}
+        </NuxtLink>
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+import axios from 'axios'
+export default {
+    async asyncData({ $axios }) {
+    const posts = await $axios.$get(`http://localhost/laravel/mini-blog/api/posts`)
+    return { posts }
+  }
+}
+</script>
